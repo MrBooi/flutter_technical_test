74359c74dc24b853a8ca0c704888c61224c2c29e
Ayabonga <ayabongabooi2@gmail.com>
2023-09-08 10:33:36 +0200

    feat(main_weather_entity): implemented main weather entity

---
+++ lib/features/current_weather/domain/entities/main_weather_entity.dart
@@ -0,0 +1,45 @@
+abstract class MainWeatherEntity {
+  final double temp;
+  final double feelsLike;
+  final double tempMin;
+  final double tempMax;
+  final int pressure;
+  final int humidity;
+
+  MainWeatherEntity({
+    required this.temp,
+    required this.feelsLike,
+    required this.tempMin,
+    required this.tempMax,
+    required this.pressure,
+    required this.humidity,
+  });
+
+  @override
+  String toString() {
+    return 'MainWeatherEntity(temp: $temp, feelsLike: $feelsLike, tempMin: $tempMin, tempMax: $tempMax, pressure: $pressure, humidity: $humidity)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is MainWeatherEntity &&
+        other.temp == temp &&
+        other.feelsLike == feelsLike &&
+        other.tempMin == tempMin &&
+        other.tempMax == tempMax &&
+        other.pressure == pressure &&
+        other.humidity == humidity;
+  }
+
+  @override
+  int get hashCode {
+    return temp.hashCode ^
+        feelsLike.hashCode ^
+        tempMin.hashCode ^
+        tempMax.hashCode ^
+        pressure.hashCode ^
+        humidity.hashCode;
+  }
+}
