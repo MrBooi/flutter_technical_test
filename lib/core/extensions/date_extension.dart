648f2bab719c662e9e388137f3fc674ac976279e
Ayabonga <ayabongabooi2@gmail.com>
2023-09-11 20:22:36 +0200

    feat(date_extension): added date extension for getweekday name

---
+++ lib/core/extensions/date_extension.dart
@@ -0,0 +1,15 @@
+extension DateTimeExtension on DateTime {
+  String get getWeekDayName {
+    final weekdays = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ];
+
+    return weekdays[weekday];
+  }
+}
