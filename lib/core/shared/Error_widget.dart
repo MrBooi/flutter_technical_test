a308bda7657775207c9f326a8b889b6082e28ab9
Ayabonga <ayabongabooi2@gmail.com>
2023-09-12 12:40:35 +0200

    feat(error_widget): added shared error widget

---
+++ lib/core/shared/Error_widget.dart
@@ -0,0 +1,20 @@
+import 'package:dvt_weather_app/core/shared/copy_text.dart';
+import 'package:flutter/material.dart';
+
+class AppErrorWidget extends StatelessWidget {
+  final String errMsg;
+  const AppErrorWidget({super.key, required this.errMsg});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Column(
+        children: [
+          CopyText(
+            errMsg,
+          )
+        ],
+      ),
+    );
+  }
+}
